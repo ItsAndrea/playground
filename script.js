d59a420f17eaf5d683afdcdc57da9bd33ebbca90
Andrea Fajardo Gutiérrez <agx8434@gmail.com>
2022-03-07 16:02:36 -0500

    Initial commit(andrea.dev)
    Creacion de variable y mensaje en la consola
    
    Ciclo for

---
+++ script.js
@@ -0,0 +1,9 @@
+let numero_magico = prompt("Dime un numero");
+console.log("Mensaje desde consola");
+
+const array = [1,2,3,4,5,6,7,8,9,10]
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    console.log(element)
+}
