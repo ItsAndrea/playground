3eb5d33d330884f0b25b41f44b5ddeca48059d0c
Andrea Fajardo Gutiérrez <agx8434@gmail.com>
2022-03-07 16:21:40 -0500

    Guardar cambios

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let numero_magico = prompt("Dime un numero");
 console.log("Mensaje desde consola");
 
-const array = [1,2,3,4,5,6,7,8,9,10]
+const array = [1,2,3,4,5,6,7,8,9,10];
 
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
-    console.log(element)
+    console.log(element);
 }
+
+let parrafo = "lorem ipsum";
